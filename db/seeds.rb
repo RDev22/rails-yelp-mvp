ac3a0a6e6d7cb53ff0833b91c0c3088579cb484e
Renan Dantas <rsdantas7@gmail.com>
2022-05-25 23:50:46 -0300

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Wiping seeds"
+Restaurant.destroy_all
+
+puts "Creating seeds"
+
+vintao = Restaurant.create(name: "vintao", address: "Rua Fidalga", phone_number: "11 3865 0000", category: "chinese")
+mc_donalds = Restaurant.create(name: "Mc Donalds", address: "Em todo lugar", phone_number: "11 3333 3333", category: "italian")
+trintao = Restaurant.create(name: "Trintao", address: "Rua Morato Coelho", phone_number: "11 9999 9999", category: "japanese")
+le_wagon = Restaurant.create(name: "Le Wagon le restaurant", address: "Rua Jerico", phone_number: "11 9899 9898", category: "french")
+nome1 = Restaurant.create(name: "Pé pra fora", address: "Rua Harmonia", phone_number: "11 3333 2222", category: "belgian")
